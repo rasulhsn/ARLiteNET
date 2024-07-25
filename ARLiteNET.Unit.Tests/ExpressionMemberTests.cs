d0e6448439df1c8a5335cf5bd482e577d3c45c86
Rasul <resulhsn@gmail.com>
2024-07-25 21:14:33 +0300

    add unit tests for expression member

---
+++ ARLiteNET.Unit.Tests/ExpressionMemberTests.cs
@@ -0,0 +1,23 @@
+﻿using ARLiteNET.Core;
+using ARLiteNET.Lib.Unit.Tests.Stub;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq.Expressions;
+
+namespace ARLiteNET.Unit.Tests
+{
+    [TestClass]
+    public class ExpressionMemberTests
+    {
+        [TestMethod]
+        public void Create_WhenGivenValidPropertyExpression_ShouldReturnPropertyNameByModel()
+        {
+            const string expected = nameof(UserDtoStub.Id);
+            Expression<Func<UserDtoStub, long>> lambda = (x) => x.Id;
+
+            ExpressionMember vMember = ExpressionMember.Create(lambda);
+
+            Assert.AreEqual(expected, vMember.Name);
+        }
+    }
+}
