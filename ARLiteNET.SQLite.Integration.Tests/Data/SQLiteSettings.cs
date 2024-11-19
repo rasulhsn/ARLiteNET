0625907f144fac23b06f0f7684e6f527f97c3b75
Rasul <resulhsn@gmail.com>
2024-11-19 21:25:23 +0400

    Fix constants

--- ARLiteNET.SQLite.Integration.Tests/Data/SQLiteSettings.cs
+++ ARLiteNET.SQLite.Integration.Tests/Data/SQLiteSettings.cs
@@ -3,9 +3,9 @@ namespace ARLiteNET.SQLite.Integration.Tests.Data
 {
     public class SQLiteSettings
     {
-        public static string DatabaseName { get; } = "InMemoryARLiteNET";
-        public static string DefaultTableName { get; } = "Users";
-        public static string ConnectionStringVersion3 { get; } = $"Data Source={DatabaseName};Mode=Memory;Cache=Shared;Version=3;";
+        public static string DatabaseName => "InMemoryARLiteNET";
+        public static string DefaultTableName => "Users";
+        public static string ConnectionStringVersion3 => $"Data Source={DatabaseName};Mode=Memory;Cache=Shared;Version=3;";
         
         public static string GetDefaultTableScript() => $"CREATE TABLE \"{DefaultTableName}\" (\r\n\t\"{nameof(SQLiteDefaultTableObject.Id)}\"\tINTEGER NOT NULL,\r\n\t\"{nameof(SQLiteDefaultTableObject.Name)}\"\tTEXT,\r\n\t\"{nameof(SQLiteDefaultTableObject.IsActive)}\"\tINTEGER NOT NULL,\r\n\tPRIMARY KEY(\"Id\")\r\n)";
         public static string GetDefaultTableInsertScript() => $"INSERT INTO {DefaultTableName} ({nameof(SQLiteDefaultTableObject.Id)},{nameof(SQLiteDefaultTableObject.Name)},{nameof(SQLiteDefaultTableObject.IsActive)}) VALUES (1, 'Rasul', 1), (2, 'Huseynov', 1)";
--- ARLiteNET.SQLite/ARLiteConnectionStringBuilderExtensions.cs
+++ ARLiteNET.SQLite/ARLiteConnectionStringBuilderExtensions.cs
@@ -3,11 +3,13 @@ namespace ARLiteNET.SQLite
 {
     public static class ARLiteConnectionStringBuilderExtensions
     {
+        const string PROVIDER_NAME_STR = "System.Data.SQLite";
+
         /// <summary>
         /// Add SQLite provider for ADO.NET access.
         /// </summary>
         public static ARLiteConnectionStringBuilder AddSQLiteProvider(this ARLiteConnectionStringBuilder connectionStringBuilder) 
-            => connectionStringBuilder.AddProvider("System.Data.SQLite", System.Data.SQLite.SQLiteFactory.Instance);
+            => connectionStringBuilder.AddProvider(PROVIDER_NAME_STR, System.Data.SQLite.SQLiteFactory.Instance);
 
         /// <summary>
         /// Add SQLite version of 3 with provider for ADO.NET access.
